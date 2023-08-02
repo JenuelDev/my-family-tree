a73e8fa55e3e22715761512cd2dbcc05f877f8bc
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-08-02 13:30:24 +0800

    Update DeleteAccount.vue

--- src/views/Instructions/DeleteAccount.vue
+++ src/views/Instructions/DeleteAccount.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 </script>
 <template>
     <div class="max-w-350px mx-auto mt-3">
-        <div class="flex items-center gap-2 p-2 shadow-none hover:shadow-md cursor-pointer select-none mb-5">
+        <div
+            class="flex items-center gap-2 p-2 shadow-none hover:shadow-md cursor-pointer select-none mb-5"
+            @click="router.push('/')"
+        >
             <Icon icon="ion:chevron-back-sharp" />
             Back
         </div>
