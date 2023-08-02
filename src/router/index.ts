2dbbaa7f2d4eacdc8bf1f497f201824ef51ead74
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-08-02 09:31:29 +0800

    Add delete instruction url

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,30 +3,41 @@ import LoginView from "@/views/LoginView.vue"
 import MainView from "@/views/Main/MainView.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/main',
-      component: MainView,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '',
-          name: 'dashboard',
-          component: () => import('@/views/Main/Dashboard/DashboardView.vue')
+            path: "/",
+            name: 'login',
+            component: LoginView
         },
         {
-          path: 'view/:id',
-          name: 'view-family-tree',
-          component: () => import('@/views/Main/ViewTree/FamilyTreeView.vue')
+            path: '/main',
+            component: MainView,
+            children: [
+                {
+                    path: '',
+                    name: 'dashboard',
+                    component: () => import('@/views/Main/Dashboard/DashboardView.vue')
+                },
+                {
+                    path: 'view/:id',
+                    name: 'view-family-tree',
+                    component: () => import('@/views/Main/ViewTree/FamilyTreeView.vue')
+                }
+            ]
+        },
+        {
+            path: "/instructions",
+            component: () => import('@/views/Instructions/Main.vue'),
+            children: [
+                {
+                    path: 'delete-account',
+                    name: 'instruction-delete-account',
+                    component: () => import('@/views/Instructions/DeleteAccount.vue')
+                }
+            ]
         }
-      ]
-    },
-  ]
+    ]
 })
 
 export default router
---
+++ src/views/Instructions/DeleteAccount.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+<template>
+    <div class="max-w-350px mx-auto mt-3">
+        <div class="flex items-center gap-2 p-2 shadow-none hover:shadow-md cursor-pointer select-none mb-5">
+            <Icon icon="ion:chevron-back-sharp" />
+            Back
+        </div>
+        <div class="px-10px">
+            <h2>How To Delete Account?</h2>
+            Do delete you account on this platform. Please send a request to this email address
+            <b>`jenuelganawed936@gmail.com`</b>
+        </div>
+    </div>
+</template>
