4bcd0af50c13b12774b50faf6ed0a08aaf23952e
Jenuel Ganawed <jenuelganawed936@gmail.com>
2024-02-17 11:30:13 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
+<script setup lang="ts">
+import LogoPng from "@/assets/Logo/Logo.png";
+const logo = LogoPng;
+</script>
 <template>
     <section class="text-gray-600 body-font">
         <div class="container mx-auto flex px-5 py-14 items-center justify-center flex-col">
             <img
                 class="lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded"
                 alt="hero"
-                src="@/assets/Logo/Logo.png"
+                :src="logo"
             />
             <div class="text-center lg:w-2/3 w-full">
                 <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
